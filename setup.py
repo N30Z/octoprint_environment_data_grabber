ba079a50c75c7045a434e3bfc40393bbd5890e43
N30Z <33665573+N30Z@users.noreply.github.com>
2024-05-26 22:45:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_environment_data_grabber"
 plugin_name = "environment_data_grabber"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.0.1"
+plugin_version = "0.0.2"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
