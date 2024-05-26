e124da5a211804c40da99120de08fb88038259e9
N30Z <33665573+N30Z@users.noreply.github.com>
2024-05-26 22:56:20 +0200

    Update environment_data_grabber.js

--- octoprint_environment_data_grabber/static/js/environment_data_grabber.js
+++ octoprint_environment_data_grabber/static/js/environment_data_grabber.js
@@ -1,30 +1,3 @@
-$(function() {
-    function EnvironmentDataViewModel(parameters) {
-        var self = this;
-
-        self.htmlContent = ko.observable("");
-        self.error = ko.observable("");
-
-        // Receive data from backend
-        self.onDataUpdaterPluginMessage = function(plugin, data) {
-            if (plugin !== "environment_data_grabber") {
-                return;
-            }
-
-            if (data.error) {
-                self.error(data.error);
-                self.htmlContent("");
-            } else {
-                self.error("");
-                self.htmlContent(data.html_content);
-            }
-        };
-    }
-
-    // Register the view model
-    OCTOPRINT_VIEWMODELS.push({
-        construct: EnvironmentDataViewModel,
-        dependencies: [],
-        elements: ["#environment_data_grabber_plugin"]
-    });
-});
+.container {
+    padding: 10px;
+}
